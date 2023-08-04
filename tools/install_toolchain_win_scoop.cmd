771bc9184ea4fd6452dd495bee2c03235e1a4c62
v.mezentsev <v.mezentsev@air-burg.ru>
2023-08-04 18:07:04 +0500

    обновлён скрипт установки окружения сборки для Windows

--- tools/install_toolchain_win_scoop.cmd
+++ tools/install_toolchain_win_scoop.cmd
@@ -1,14 +1,20 @@
+:: Установка scoop.
 powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;
 powershell "irm get.scoop.sh | iex"
 
+:: Добавление репозиториев.
 powershell scoop bucket add main
 powershell scoop bucket add extras
 
-powershell scoop install windows-terminal
-powershell scoop install pwsh
+::powershell scoop install windows-terminal
+::powershell scoop install pwsh
+::powershell scoop install gdb
+::powershell scoop install make
 
 powershell scoop install ninja
 powershell scoop install cmake
 powershell scoop install gcc-arm-none-eabi
-powershell scoop install gdb
+powershell scoop install openocd
+powershell scoop install srecord
 powershell scoop install stlink
+
