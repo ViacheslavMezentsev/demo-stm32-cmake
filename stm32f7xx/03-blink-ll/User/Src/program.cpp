e8efb57a9c9bf176737351f2ec8b15c0ba2bfff0
v.mezentsev <v.mezentsev@air-burg.ru>
2023-08-17 15:28:15 +0500

    добавлен stm32-cmake проект на основе LL драйверов

---
+++ stm32f7xx/03-blink-ll/User/Src/program.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/// Semihosting Initializing.
+extern "C" void initialise_monitor_handles( void );
+
+/**
+ * \brief   Выполняет инициализацию.
+ *
+ */
+void init( void )
+{
+    // Инициализация библиотеки Semihosting.
+    initialise_monitor_handles();
+}
+
+
+/**
+ * \brief   Выполняет дополнительные настройки.
+ *
+ */
+void setup( void )
+{
+    LL_GPIO_ResetOutputPin( LED_USER_GPIO_Port, LED_USER_Pin );
+}
+
+
+/**
+ * \brief   Выполняется периодически в теле основного цикла.
+ *
+ */
+void loop( void )
+{    
+    // Переключаем выход порта (мигаем светодиодом).
+    LL_GPIO_TogglePin( LED_USER_GPIO_Port, LED_USER_Pin );    
+
+    auto value = LL_GPIO_IsInputPinSet( LED_USER_GPIO_Port, LED_USER_Pin );
+
+    printf( "Led %s\n", value == SET ? "On" : "Off" );
+
+    LL_mDelay( 500 );
+}
