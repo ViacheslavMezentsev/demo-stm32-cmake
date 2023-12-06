fe7e29025b1291f10c1f29024bb2102de6dee43f
viacheslavmezentsev <viacheslavmezentsev@ya.ru>
2023-12-06 16:18:56 +0500

    доработан проект modm-blink для blue-pill (stm32f103)

--- stm32f1xx/modm-blink/main.cpp
+++ stm32f1xx/modm-blink/main.cpp
@@ -20,39 +20,41 @@ using namespace modm::platform;
  */
 int main()
 {
-    //Board::initialize();
-
+#if 1
+    Board::initialize();
+#else
     Rcc::enableInternalClock();
 
     // switch system clock to PLL output
-    Rcc::enableSystemClock(Rcc::SystemClockSource::InternalClock);
+    Rcc::enableSystemClock( Rcc::SystemClockSource::InternalClock );
 
     // AHB has max 8MHz
-    Rcc::setAhbPrescaler(Rcc::AhbPrescaler::Div1);
+    Rcc::setAhbPrescaler( Rcc::AhbPrescaler::Div1 );
 
     // APB1 has max. 8MHz
-    Rcc::setApb1Prescaler(Rcc::Apb1Prescaler::Div1);
+    Rcc::setApb1Prescaler( Rcc::Apb1Prescaler::Div1 );
 
     // APB2 has max. 8MHz
-    Rcc::setApb2Prescaler(Rcc::Apb2Prescaler::Div1);
+    Rcc::setApb2Prescaler( Rcc::Apb2Prescaler::Div1 );
 
     // update frequencies for busy-wait delay functions
     Rcc::updateCoreFrequency<Rcc::BootFrequency>();
 
-    extern void modm_dwt_enable(void);
+    extern void modm_dwt_enable( void );
 
     modm_dwt_enable();
+#endif
 
     LedGreen::reset();
 
     while ( true )
     {
         LedGreen::set();
-        
+
         modm::delay( 500ms );
 
         LedGreen::reset();
-        
+
         modm::delay( 500ms );
     }
 }
