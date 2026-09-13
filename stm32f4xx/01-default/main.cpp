0eaf00af7378ba93d74205d60fc98493b6632f2a
ViacheslavMezentsev <viacheslavmezentsev@ya.ru>
2026-09-13 16:25:23 +0500

    небольшие эксперименты с stm32f4xx/01-default

--- stm32f4xx/01-default/main.cpp
+++ stm32f4xx/01-default/main.cpp
@@ -10,6 +10,52 @@ extern "C" void SysTick_Handler( void )
     HAL_IncTick();
 }
 
+/**
+ * \brief   Настройка системного тактирования.
+ *
+ * Настройка тактирования на частоту 16 MHz с использованием внутреннего генератора HSI.
+ */
+void SystemClock_Config( void )
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+    // Configure the main internal regulator output voltage.
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG( PWR_REGULATOR_VOLTAGE_SCALE1 );
+
+    // Настройка источника тактирования (HSI).
+    RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_NONE;    // PLL отключен
+
+    if ( HAL_RCC_OscConfig( &RCC_OscInitStruct ) != HAL_OK )
+    {
+        // Ошибка конфигурации.
+        while ( 1 )
+        {
+        }
+    }
+
+    // Настройка системных шин.
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_HSI;    // SYSCLK = HSI (16 MHz)
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;         // HCLK = 16 MHz
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;           // APB1 = 16 MHz (max 45 MHz)
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;           // APB2 = 16 MHz (max 90 MHz)
+
+    // Настройка латентности Flash.
+    // Для 16 MHz достаточно 0 wait states (диапазон 0-16 MHz при 3.3V)
+    if ( HAL_RCC_ClockConfig( &RCC_ClkInitStruct, FLASH_LATENCY_0 ) != HAL_OK )
+    {
+        // Ошибка конфигурации.
+        while ( 1 )
+        {
+        }
+    }
+}
 
 /**
  * \brief   Выполняет инициализацию портов ввода-вывода.
@@ -42,6 +88,9 @@ int main( void )
 {
     // Инициализация библиотеки HAL.
     HAL_Init();
+
+    // Настройка системного тактирования.
+    SystemClock_Config();
 
     // Инициализация портов ввода-вывода.
     initGPIO();
